403a6c1fedd27bc6fdba9ac9f3ba4da85bbd0486
h28a3 <at.h.s_24328@iCloud.com>
2024-07-30 12:50:46 +0900

    Add files via upload

---
+++ header.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+extern int deck[13]; //RD‹L˜^—p•Ï”
+
+void initialization();
+int sum_card(vector<int> a);
+int draw();
